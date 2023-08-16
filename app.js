7b3b972fcef2e0e3cd5e3e2593df087f8346d0b5
kutsarvasya <vkutsar89@gmail.com>
2023-08-16 09:32:46 +0200

    add hooks

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import authRouter from "./routes/api/auth-router.js";
-
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/auth", authRouter);
 
 app.use((req, res) => {
